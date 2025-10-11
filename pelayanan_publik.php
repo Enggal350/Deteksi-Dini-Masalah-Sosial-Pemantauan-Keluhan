f7b3dc8227f68b5665b0c76f3a6f34fd2dac81f1
Fadilah Safitri <fadilahfitri41@gmail.com>
2025-10-11 08:13:25 +0700

    Update pelayanan_publik.php

--- pelayanan_publik.php
+++ pelayanan_publik.php
@@ -8,27 +8,21 @@
       <table class="table table-striped table-hover align-middle text-center">
         <thead class="table-success">
           <tr>
-            <th>No</th>
-            <th>Nama</th>
+           
             <th>Kategori</th>
             <th>Isi Aduan</th>
-            <th>Tanggal</th>
-            <th>Status</th>
-            <th>Aksi</th>
+           
           </tr>
           </thead>
           <?php 
           foreach($data as $item) {
-            if ($item['kategori'] === 'Pelayanan Publik'):
+            if ($item['kategori'] === 'Pelayanan Publik' ):
           ?>
           <tr>
-            <th></th>
-            <th></th>
+          
             <th><?php echo $item ['kategori']?> </th>
             <th><?php echo $item ['text']?></th>
-            <th></th>
-            <th></th>
-            <th></th>
+            
           </tr>
           <?php endif; ?>
           <?php } ?>
